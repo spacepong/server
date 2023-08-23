6aa23045f4e42a9bab88c71c309b527ef0f564a8
Abdel <abdelisloading@gmail.com>
2023-08-23 23:48:17 +0100

    Improve user blocking system

--- src/user/services/user-relations.service.ts
+++ src/user/services/user-relations.service.ts
@@ -22,9 +22,17 @@ export class UserRelationsService {
    * @throws {NotFoundException} If the user or the user to follow is not found.
    */
   async followUser(userId: string, followId: string): Promise<User> {
+    /**
+     * Check if the user is trying to follow themselves.
+     * If they are, throw a ForbiddenException.
+     */
     if (userId === followId)
       throw new ForbiddenException('Cannot follow yourself');
 
+    /**
+     * Find the user and the user to follow.
+     * If either of them is not found, throw a NotFoundException.
+     */
     const user: User = await this.prisma.user.findUnique({
       where: {
         id: userId,
@@ -39,11 +47,38 @@ export class UserRelationsService {
 
     if (!user || !toFollow) throw new NotFoundException('User not found');
 
+    /**
+     * Check if the user is already following the user to follow.
+     * If they are, return the user unchanged.
+     * If they aren't, update the user and return the updated user.
+     */
     const isAlreadyFollowing: boolean = user.following.some(
       (followedUser: string) => followedUser === followId,
     );
 
     if (!isAlreadyFollowing) {
+      /**
+       * Check if the user is blocked by the user to follow.
+       * If they are, throw a ForbiddenException.
+       */
+      const isBlocked: boolean = toFollow.blocked.some(
+        (blockedUser: string) => blockedUser === userId,
+      );
+
+      if (isBlocked)
+        throw new ForbiddenException('Cannot follow a user who blocked you');
+
+      /**
+       * Check if the user to follow is blocked by the user.
+       * If they are, throw a ForbiddenException.
+       */
+      const isBlockedByUser: boolean = user.blocked.some(
+        (blockedUser: string) => blockedUser === followId,
+      );
+
+      if (isBlockedByUser)
+        throw new ForbiddenException('Cannot follow a user you blocked');
+
       return this.prisma.user.update({
         where: {
           id: userId,
@@ -68,9 +103,17 @@ export class UserRelationsService {
    * @throws {NotFoundException} If the user or the user to unfollow is not found.
    */
   async unfollowUser(userId: string, unfollowId: string): Promise<User> {
+    /**
+     * Check if the user is trying to unfollow themselves.
+     * If they are, throw a ForbiddenException.
+     */
     if (userId === unfollowId)
       throw new ForbiddenException('Cannot unfollow yourself');
 
+    /**
+     * Find the user and the user to unfollow.
+     * If either of them is not found, throw a NotFoundException.
+     */
     const user: User = await this.prisma.user.findUnique({
       where: {
         id: userId,
@@ -85,6 +128,11 @@ export class UserRelationsService {
 
     if (!user || !toUnfollow) throw new NotFoundException('User not found');
 
+    /**
+     * Check if the user is already following the user to unfollow.
+     * If they are, update the user and return the updated user.
+     * If they aren't, return the user unchanged.
+     */
     const isAlreadyFollowing: boolean = user.following.some(
       (followedUser: string) => followedUser === unfollowId,
     );
@@ -116,9 +164,17 @@ export class UserRelationsService {
    * @throws {NotFoundException} If the user or the user to block is not found.
    */
   async blockUser(userId: string, blockId: string): Promise<User> {
+    /**
+     * Check if the user is trying to block themselves.
+     * If they are, throw a ForbiddenException.
+     */
     if (userId === blockId)
       throw new ForbiddenException('Cannot block yourself');
 
+    /**
+     * Find the user and the user to block.
+     * If either of them is not found, throw a NotFoundException.
+     */
     const user: User = await this.prisma.user.findUnique({
       where: {
         id: userId,
@@ -133,11 +189,36 @@ export class UserRelationsService {
 
     if (!user || !toBlock) throw new NotFoundException('User not found');
 
+    /**
+     * Check if the user is already blocked.
+     * If they are, return the user unchanged.
+     * If they aren't, update the user and return the updated user.
+     */
     const isAlreadyBlocked: boolean = user.blocked.some(
       (blockedUser: string) => blockedUser === blockId,
     );
 
     if (!isAlreadyBlocked) {
+      /**
+       * Check if the user is following the user to block.
+       * If they are, unfollow the user to block.
+       */
+      if (
+        user.following.some((followedUser: string) => followedUser === blockId)
+      )
+        await this.unfollowUser(userId, blockId);
+
+      /**
+       * Check if the user to block is following the user.
+       * If they are, unfollow the user.
+       */
+      if (
+        toBlock.following.some(
+          (followedUser: string) => followedUser === userId,
+        )
+      )
+        await this.unfollowUser(blockId, userId);
+
       return this.prisma.user.update({
         where: {
           id: userId,
