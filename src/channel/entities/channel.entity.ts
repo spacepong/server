0b001cb477b33c1e3443fb3a0cb69328b1e82ac1
Abdel <abdelisloading@gmail.com>
2023-08-30 13:42:42 +0100

    Improve and comment the channel and message entities

--- src/channel/entities/channel.entity.ts
+++ src/channel/entities/channel.entity.ts
@@ -1,10 +1,20 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 import { User } from 'src/user/entities/user.entity';
 import { Mute } from 'src/mute/entities/mute.entity';
 import { Kick } from 'src/kick/entities/kick.entity';
 import { Message } from 'src/message/entities/message.entity';
 
+/**
+ * Represents the type of a channel.
+ * @export
+ * @enum {string}
+ * @property {string} PUBLIC 'PUBLIC' - A public channel.
+ * @property {string} PRIVATE 'PRIVATE' - A private channel.
+ * @property {string} PROTECTED 'PROTECTED' - A protected channel.
+ * @property {string} DIRECT 'DIRECT' - A direct channel.
+ */
 export enum ChannelType {
   PUBLIC = 'PUBLIC',
   PRIVATE = 'PRIVATE',
@@ -12,19 +22,148 @@ export enum ChannelType {
   DIRECT = 'DIRECT',
 }
 
+/**
+ * Represents an individual channel within the system.
+ * This entity encapsulates fundamental channel-related details.
+ *
+ * @export
+ * @class Channel
+ */
 @ObjectType()
 export class Channel {
+  /**
+   * The unique identifier for the channel.
+   * @type {string}
+   */
+  @IsNotEmpty({ message: 'Channel ID must not be empty' })
+  @Field(() => String, { description: 'Unique identifier for the channel' })
   id: string;
+
+  /**
+   * The name of the channel.
+   * @type {string}
+   */
+  @IsNotEmpty({ message: 'Channel name must not be empty' })
+  @IsString({ message: 'Channel name must be a string' })
+  @Field(() => String, { description: 'Name of the channel' })
   name: string;
+
+  /**
+   * The description of the channel.
+   * @type {string}
+   */
+  @IsString({ message: 'Channel description must be a string' })
+  @Field(() => String, {
+    description: 'Description of the channel',
+    nullable: true,
+  })
   description?: string;
+
+  /**
+   * The type of the channel.
+   * @type {ChannelType}
+   */
+  @IsNotEmpty({ message: 'Channel type must not be empty' })
+  @Field(() => ChannelType, {
+    description: 'Type of the channel',
+  })
   type: ChannelType;
+
+  /**
+   * The hashed password of the channel (if applicable).
+   * Applies to protected channels.
+   * @type {string}
+   */
+  @IsString({ message: 'Channel password must be a string' })
+  @Field(() => String, {
+    description: 'Password of the channel',
+    nullable: true,
+  })
   password?: string;
-  ownerId: string;
+
+  /**
+   * The owner of the channel.
+   * @type {string}
+   */
+  @IsNotEmpty({ message: 'Channel owner ID must not be empty' })
+  @IsString({ message: 'Channel owner ID must be a string' })
+  @Field(() => String, {
+    description: 'Unique identifier for the channel owner',
+    nullable: true,
+  })
+  ownerId?: string;
+
+  /**
+   * The admins of the channel.
+   * @type {string[]}
+   */
+  @IsNotEmpty({ message: 'Channel admin IDs must not be empty' })
+  @IsString({ each: true, message: 'Channel admin IDs must be a string' })
+  @Field(() => [String], {
+    description: 'Unique identifiers for the channel admins',
+  })
   adminIds: string[];
+
+  /**
+   * The muted users of the channel.
+   * @type {Mute[]}
+   * @default []
+   */
+  @Field(() => [Mute], {
+    description: 'Mutes associated with the channel',
+    defaultValue: [],
+  })
   mutes: Mute[];
+
+  /**
+   * The kicked users of the channel.
+   * @type {Kick[]}
+   * @default []
+   */
+  @Field(() => [Kick], {
+    description: 'Kicks associated with the channel',
+    defaultValue: [],
+  })
   kicks: Kick[];
+
+  /**
+   * The members of the channel.
+   * @type {User[]}
+   * @default []
+   */
+  @IsNotEmpty({ message: 'Channel users must not be empty' })
+  @Field(() => [User], {
+    description: 'Members of the channel',
+    defaultValue: [],
+  })
   users: User[];
+
+  /**
+   * The messages of the channel.
+   * @type {Message[]}
+   * @default []
+   */
+  @Field(() => [Message], {
+    description: 'Messages associated with the channel',
+    defaultValue: [],
+  })
   messages: Message[];
+
+  /**
+   * The date the channel was created.
+   * @type {Date}
+   */
+  @IsNotEmpty({ message: 'Channel created at must not be empty' })
+  @IsDate({ message: 'Channel created at must be a date' })
+  @Field(() => Date, { description: 'Date the channel was created' })
   createdAt: Date;
+
+  /**
+   * The date the channel was last updated.
+   * @type {Date}
+   */
+  @IsNotEmpty({ message: 'Channel updated at must not be empty' })
+  @IsDate({ message: 'Channel updated at must be a date' })
+  @Field(() => Date, { description: 'Date the channel was last updated' })
   updatedAt: Date;
 }
--- src/message/entities/message.entity.ts
+++ src/message/entities/message.entity.ts
@@ -1,18 +1,138 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 import { User } from 'src/user/entities/user.entity';
 import { Channel } from 'src/channel/entities/channel.entity';
 
+/**
+ * Represents an individual message within the system.
+ * This entity encapsulates fundamental message-related details.
+ *
+ * @export
+ * @class Message
+ */
 @ObjectType()
 export class Message {
+  /**
+   * The unique identifier for the message.
+   * @type {string}
+   */
+  @IsNotEmpty({ message: 'Message ID must not be empty' })
+  @Field(() => String, { description: 'Unique identifier for the message' })
   id: string;
+
+  /**
+   * The ID of the associated channel.
+   * @type {string}
+   */
+  @IsNotEmpty({ message: 'Channel ID must not be empty' })
+  @IsString({ message: 'Channel ID must be a string' })
+  @Field(() => String, { description: 'ID of the associated channel' })
   channelId: string;
+
+  /**
+   * The associated channel entity.
+   * @type {Channel}
+   */
+  @Field(() => Channel, {
+    description: 'The associated channel entity',
+  })
   channel?: Channel;
+
+  /**
+   * The ID of the associated user.
+   * @type {string}
+   */
+  @IsNotEmpty({ message: 'User ID must not be empty' })
+  @IsString({ message: 'User ID must be a string' })
+  @Field(() => String, { description: 'ID of the associated user' })
   userId: string;
+
+  /**
+   * The associated user entity.
+   * @type {User}
+   */
+  @Field(() => User, {
+    description: 'The associated user entity',
+  })
   user?: User;
-  content: string;
+
+  /**
+   * The text of the message.
+   * @type {string}
+   */
+  @IsString({ message: 'Message text must be a string' })
+  @Field(() => String, {
+    description: 'Text of the message',
+    nullable: true,
+  })
+  text?: string;
+
+  /**
+   * The photo of the message.
+   * @type {string}
+   */
+  @IsString({ message: 'Message photo must be a string' })
+  @Field(() => String, {
+    description: 'Photo of the message',
+    nullable: true,
+  })
+  photo?: string;
+
+  /**
+   * The invite to a game.
+   * @type {string}
+   */
+  @IsString({ message: 'Message invite must be a string' })
+  @Field(() => String, {
+    description: 'Invite to a game',
+    nullable: true,
+  })
+  invite?: string;
+
+  /**
+   * Whether or not the message is unsent.
+   * @type {boolean}
+   * @default false
+   */
+  @IsNotEmpty({ message: 'Message is unsent must not be empty' })
+  @IsBoolean({ message: 'Message is unsent must be a boolean' })
+  @Field(() => Boolean, {
+    description: 'Whether or not the message is unsent',
+    defaultValue: false,
+  })
   unsent: boolean;
+
+  /**
+   * The IDs of the users who have seen the message.
+   * @type {string[]}
+   * @default []
+   */
+  @IsNotEmpty({ message: 'Message seenBy must not be empty' })
+  @IsString({ each: true, message: 'Message seenBy must be a string' })
+  @Field(() => [String], {
+    description: 'IDs of the users who have seen the message',
+    defaultValue: [],
+  })
   seenBy: string[];
+
+  /**
+   * The date when the message was created.
+   * @type {Date}
+   */
+  @IsNotEmpty({ message: 'Created at must not be empty' })
+  @IsDate({ message: 'Created at must be a valid date' })
+  @Field(() => Date, { description: 'The date when the message was created' })
   createdAt: Date;
+
+  /**
+   * The date when the message was last updated.
+   * @type {Date}
+   */
+  @IsNotEmpty({ message: 'Updated at must not be empty' })
+  @IsDate({ message: 'Updated at must be a valid date' })
+  @Field(() => Date, {
+    description: 'The date when the message was last updated',
+  })
   updatedAt: Date;
 }
