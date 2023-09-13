512ff2ac342c02e8d3afdad8a3986d2540f90fa2
Abdel <abdelisloading@gmail.com>
2023-09-13 09:16:11 +0100

    Add chat-related socket events

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -15,57 +15,319 @@ import {
   WebsocketMiddleware,
 } from 'src/auth/middlewares/websocket.middleware';
 import { ChatService } from './chat.service';
-import { WebsocketGuard } from 'src/auth/guards/websocket.guard';
+import { ChannelService } from 'src/channel/channel.service';
+import {
+  WebsocketGuard,
+  getClientHandshake,
+} from 'src/auth/guards/websocket.guard';
+import { Channel } from 'src/channel/entities/channel.entity';
+import { isEmpty } from 'src/utils/is-empty';
 
 @WebSocketGateway({ cors: true })
 @UseGuards(WebsocketGuard)
 export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-  constructor(private readonly chatService: ChatService) {}
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly channelService: ChannelService,
+  ) {}
 
   @WebSocketServer()
-  private server: Server;
+  private readonly server: Server;
 
-  afterInit(client: Socket) {
+  public afterInit(client: Socket) {
     client.use(WebsocketMiddleware() as SocketIOMiddleware as any);
   }
 
-  handleConnection(client: Socket) {
-    client;
+  public handleConnection(client: Socket) {
+    const userId: string = this.chatService.addUser(
+      client,
+      getClientHandshake(client, 'userId'),
+    );
+    if (!userId) return;
+
+    this.server.emit('users', this.chatService.getUserIds());
+
+    this.channelService
+      .getChannelsByUserId(userId)
+      .then((channels: Channel[]) => {
+        channels.forEach((channel: Channel) => {
+          client.join(channel.id);
+        });
+      });
   }
 
-  handleDisconnect(client: Socket): WsResponse<void> {
+  public handleDisconnect(client: Socket): WsResponse<void> {
     const userId: string = this.chatService.removeUser(client.id);
     if (!userId) return;
 
     this.server.emit('users', this.chatService.getUserIds());
-    this.server.emit('message', {
-      userId: 'system',
-      message: `${userId} has left the chat`,
-    });
+
+    this.channelService
+      .getChannelsByUserId(userId)
+      .then((channels: Channel[]) => {
+        channels.forEach((channel: Channel) => {
+          client.leave(channel.id);
+        });
+      });
   }
 
   @SubscribeMessage('message')
-  handleMessage(client: Socket, payload: { data: string }): WsResponse<void> {
+  public handleMessage(
+    client: Socket,
+    payload: { roomId: string; message: string },
+  ): WsResponse<void> {
     const userId: string = this.chatService.getUserId(client.id);
     if (!userId) return;
 
-    this.server.emit('message', {
-      userId,
-      message: payload.data,
-    });
+    if (isEmpty(payload.roomId))
+      client.emit('error', {
+        message: 'Room ID is required',
+      });
+    else if (isEmpty(payload.message))
+      client.emit('error', {
+        message: 'Message cannot be empty',
+      });
+    else if (!client.rooms.has(payload.roomId))
+      client.emit('error', {
+        message: 'You are not in this channel',
+      });
+    else
+      this.server.to(payload.roomId).emit('message', {
+        userId,
+        message: payload.message,
+      });
   }
 
-  @SubscribeMessage('add-user')
-  handleAddUser(client: Socket, payload: { userId: string }): WsResponse<void> {
-    const userId: string = this.chatService.addUser(client.id, payload.userId);
+  @SubscribeMessage('typing')
+  public handleTyping(
+    client: Socket,
+    payload: { roomId: string; typing: boolean },
+  ): WsResponse<void> {
+    const userId: string = this.chatService.getUserId(client.id);
     if (!userId) return;
 
-    this.server.emit('users', this.chatService.getUserIds());
-    this.server.emit('message', {
-      userId: 'system',
-      message: `${payload.userId} has joined the chat`,
-    });
+    if (isEmpty(payload.roomId))
+      client.emit('error', {
+        message: 'Room ID is required',
+      });
+    else if (!client.rooms.has(payload.roomId))
+      client.emit('error', {
+        message: 'You are not in this channel',
+      });
+    else
+      this.server.to(payload.roomId).emit('typing', {
+        userId,
+        typing: payload.typing,
+      });
+  }
+
+  @SubscribeMessage('mute')
+  public handleMute(
+    client: Socket,
+    payload: {
+      roomId: string;
+      userId: string;
+      duration: number;
+      reason?: string;
+    },
+  ): WsResponse<void> {
+    const userId: string = this.chatService.getUserId(client.id);
+    if (!userId) return;
+
+    if (isEmpty(payload.roomId))
+      client.emit('error', {
+        message: 'Room ID is required',
+      });
+    else if (isEmpty(payload.userId))
+      client.emit('error', {
+        message: 'User ID is required',
+      });
+    else if (!client.rooms.has(payload.roomId))
+      client.emit('error', {
+        message: 'You are not in this channel',
+      });
+    else
+      this.server.to(payload.roomId).emit('mute', {
+        userId,
+        muted: payload.userId,
+        duration: payload.duration,
+        reason: payload.reason,
+      });
+  }
+
+  @SubscribeMessage('unmute')
+  public handleUnmute(
+    client: Socket,
+    payload: {
+      roomId: string;
+      userId: string;
+    },
+  ): WsResponse<void> {
+    const userId: string = this.chatService.getUserId(client.id);
+    if (!userId) return;
+
+    if (isEmpty(payload.roomId))
+      client.emit('error', {
+        message: 'Room ID is required',
+      });
+    else if (isEmpty(payload.userId))
+      client.emit('error', {
+        message: 'User ID is required',
+      });
+    else if (!client.rooms.has(payload.roomId))
+      client.emit('error', {
+        message: 'You are not in this channel',
+      });
+    else
+      this.server.to(payload.roomId).emit('unmute', {
+        userId,
+        unmuted: payload.userId,
+      });
+  }
+
+  @SubscribeMessage('kick')
+  public handleKick(
+    client: Socket,
+    payload: {
+      roomId: string;
+      userId: string;
+    },
+  ): WsResponse<void> {
+    const userId: string = this.chatService.getUserId(client.id);
+    if (!userId) return;
+
+    if (isEmpty(payload.roomId))
+      client.emit('error', {
+        message: 'Room ID is required',
+      });
+    else if (isEmpty(payload.userId))
+      client.emit('error', {
+        message: 'User ID is required',
+      });
+    else if (!client.rooms.has(payload.roomId))
+      client.emit('error', {
+        message: 'You are not in this channel',
+      });
+    else
+      this.server.to(payload.roomId).emit('kick', {
+        userId,
+        kicked: payload.userId,
+      });
+  }
+
+  @SubscribeMessage('ban')
+  public handleBan(
+    client: Socket,
+    payload: {
+      roomId: string;
+      userId: string;
+      reason?: string;
+    },
+  ): WsResponse<void> {
+    const userId: string = this.chatService.getUserId(client.id);
+    if (!userId) return;
+
+    if (isEmpty(payload.roomId))
+      client.emit('error', {
+        message: 'Room ID is required',
+      });
+    else if (isEmpty(payload.userId))
+      client.emit('error', {
+        message: 'User ID is required',
+      });
+    else if (!client.rooms.has(payload.roomId))
+      client.emit('error', {
+        message: 'You are not in this channel',
+      });
+    else
+      this.server.to(payload.roomId).emit('ban', {
+        userId,
+        banned: payload.userId,
+        reason: payload.reason,
+      });
+  }
+
+  @SubscribeMessage('unban')
+  public handleUnban(
+    client: Socket,
+    payload: {
+      roomId: string;
+      userId: string;
+    },
+  ): WsResponse<void> {
+    const userId: string = this.chatService.getUserId(client.id);
+    if (!userId) return;
+
+    if (isEmpty(payload.roomId))
+      client.emit('error', {
+        message: 'Room ID is required',
+      });
+    else if (isEmpty(payload.userId))
+      client.emit('error', {
+        message: 'User ID is required',
+      });
+    else if (!client.rooms.has(payload.roomId))
+      client.emit('error', {
+        message: 'You are not in this channel',
+      });
+    else
+      this.server.to(payload.roomId).emit('unban', {
+        userId,
+        unbanned: payload.userId,
+      });
+  }
+
+  @SubscribeMessage('join')
+  public handleJoin(
+    client: Socket,
+    payload: {
+      roomId: string;
+    },
+  ): WsResponse<void> {
+    const userId: string = this.chatService.getUserId(client.id);
+    if (!userId) return;
+
+    if (isEmpty(payload.roomId))
+      client.emit('error', {
+        message: 'Room ID is required',
+      });
+    else if (!client.rooms.has(payload.roomId))
+      client.emit('error', {
+        message: 'You are not in this channel',
+      });
+    else {
+      client.join(payload.roomId);
+      this.server.to(payload.roomId).emit('join', {
+        userId,
+      });
+    }
+  }
+
+  @SubscribeMessage('leave')
+  public handleLeave(
+    client: Socket,
+    payload: {
+      roomId: string;
+    },
+  ): WsResponse<void> {
+    const userId: string = this.chatService.getUserId(client.id);
+    if (!userId) return;
+
+    if (isEmpty(payload.roomId))
+      client.emit('error', {
+        message: 'Room ID is required',
+      });
+    else if (!client.rooms.has(payload.roomId))
+      client.emit('error', {
+        message: 'You are not in this channel',
+      });
+    else {
+      client.leave(payload.roomId);
+      this.server.to(payload.roomId).emit('leave', {
+        userId,
+      });
+    }
   }
 }
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,36 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+import { SocketUser } from './types/socket-user';
 
 @Injectable()
 export class ChatService {
   constructor() {}
 
-  private users: Map<string, string> = new Map();
+  private users: SocketUser[] = [];
 
-  addUser(socketId: string, userId: string): string {
-    if (this.users.has(socketId)) return;
-    this.users.set(socketId, userId);
-    return socketId;
-  }
-
-  removeUser(socketId: string): string {
-    const userId: string = this.users.get(socketId);
-    if (userId) this.users.delete(socketId);
+  addUser(socket: Socket, userId: string): string {
+    !this.users.some((user: SocketUser) => user.userId === userId) &&
+      this.users.push({ userId, sockets: [] });
+    this.users
+      .find((user: SocketUser) => user.userId === userId)
+      .sockets.push(socket);
     return userId;
   }
 
-  getUserId(socketId: string): string {
-    return this.users.get(socketId);
+  removeUser(socketId: string): string {
+    const user: SocketUser = this.users.find((user: SocketUser) =>
+      user.sockets.some((socket: Socket) => socket.id === socketId),
+    );
+    if (!user) return;
+    user.sockets = user.sockets.filter(
+      (socket: Socket) => socket.id !== socketId,
+    );
+    if (user.sockets.length === 0)
+      this.users = this.users.filter(
+        (connectedUser: SocketUser) => connectedUser.userId !== user.userId,
+      );
+    return user?.userId;
   }
 
-  getSocketId(userId: string): string {
-    return this.users.get(userId);
+  getUserId(socketId: string): string {
+    const user: SocketUser = this.users.find((user: SocketUser) =>
+      user.sockets.find((socket: Socket) => socket.id === socketId),
+    );
+    return user?.userId;
+  }
+
+  getUserSocketIds(userId: string): string[] {
+    const user: SocketUser = this.users.find(
+      (user: SocketUser) => user.userId === userId,
+    );
+    if (!user) return;
+    return user.sockets.map((socket: Socket) => socket.id);
   }
 
   getUserIds(): string[] {
-    return Array.from(this.users.values());
+    return this.users.map((user: SocketUser) => user.userId);
   }
 
   getSocketIds(): string[] {
-    return Array.from(this.users.keys());
+    return this.users.reduce(
+      (socketIds: string[], user: SocketUser) => [
+        ...socketIds,
+        ...user.sockets.map((socket: Socket) => socket.id),
+      ],
+      [],
+    );
   }
 }
